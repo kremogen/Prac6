7c8158e63741a6b04a9ebca2cd16d41193144f73
Kalganov Nikita <nik.kosachev@mail.ru>
2021-10-02 01:37:42 +0300

    Initial commit

--- src/com/kremogen/Main.java
+++ src/com/kremogen/Main.java
@@ -3,6 +3,7 @@ package com.kremogen;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int[] array = {10, 2, 4, 5, 6, 7, 8};
+        Sorting.insertionSort(array);
     }
 }
---
+++ src/com/kremogen/Sorting.java
@@ -0,0 +1,21 @@
+package com.kremogen;
+
+import java.util.Arrays;
+
+public class Sorting {
+    public static void insertionSort(int[] array) {
+        for (int left = 0; left < array.length; left++) {
+            int value = array[left];
+            int i = left - 1;
+            for (; i >= 0; i--) {
+                if (value < array[i]) {
+                    array[i + 1] = array[i];
+                } else {
+                    break;
+                }
+            }
+            array[i + 1] = value;
+        }
+        System.out.println(Arrays.toString(array));
+    }
+}
